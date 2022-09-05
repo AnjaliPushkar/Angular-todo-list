5e078a36e4fe98f469ee334ab541218a2ecf99e9
ANJALI PUSHKAR <anjalipushkar@ANJALIs-MacBook-Air.local>
2022-09-06 00:27:48 +0530

    Components added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutComponent } from './MyComponents/about/about.component';
+import { AddTodoComponent } from './MyComponents/add-todo/add-todo.component';
+import { TodoItemComponent } from './MyComponents/todo-item/todo-item.component';
+import { TodosComponent } from './MyComponents/todos/todos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    AddTodoComponent,
+    TodoItemComponent,
+    TodosComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
